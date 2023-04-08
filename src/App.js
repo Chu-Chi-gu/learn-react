c040ee01eb9d14c7146b9eb99c0069c2f4b03cab
Chu-Chi-gu <asdf4865123@gmail.com>
2023-04-09 00:28:18 +0800

    新增useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleC() {
+    setCount(count+1);
+  }
+  useEffect(() => {
+    document.title = `you clicked ${count} times`;
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count+1)}>
+        click me
+      </button>
     </div>
   );
 }
+
 
 export default App;
