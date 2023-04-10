ad74a37529fc190f200d935582514cab1c02eecf
Chu-Chi-gu <asdf4865123@gmail.com>
2023-04-11 02:21:23 +0800

    '初期切版'

---
+++ src/pages/Home/components/Item.js
@@ -0,0 +1,11 @@
+const Item = () =>{
+
+    return (
+        <div className="item">
+            123
+            <button className="remove">刪除</button>
+        </div>
+    )
+}
+
+export default Item
