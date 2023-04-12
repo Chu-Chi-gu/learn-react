ce90aceb750dc5ce4b9f284ec0b1b644a45353c3
Chu-Chi-gu <asdf4865123@gmail.com>
2023-04-12 16:52:12 +0800

    '新增刪除功能'

--- src/pages/Home/components/Item.js
+++ src/pages/Home/components/Item.js
@@ -1,11 +1,20 @@
-const Item = () =>{
+const Item = ({ id, note, date, time, deleteData }) => {
 
+    function deleteItem() {
+      deleteData(function(prev) {
+        return prev.filter(item => item.id !== id)
+      })
+    }
+  
     return (
-        <div className="item">
-            123
-            <button className="remove">刪除</button>
+      <div className="item">
+        <div>
+          <p>{note}</p>
+          <p>{`${date} ${time}`}</p>
         </div>
-    )
-}
-
-export default Item
+        <button onClick={deleteItem} className="remove">刪除</button>
+      </div>
+    );
+  };
+  
+  export default Item;
--- src/pages/Home/components/List.js
+++ src/pages/Home/components/List.js
@@ -1,15 +1,23 @@
-import Item from './Item'
+import Item from "./Item";
 
+const List = ({ listData, deleteData }) => {
+  return (
+    <div className="list">
+      {listData.map((item) => {
+        const { note, date, time, id } = item;
+        return (
+          <Item
+            key={id}
+            id={id}
+            note={note}
+            date={date}
+            time={time}
+            deleteData={deleteData}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-const List = () =>{
-
-    return (
-        <div className="list">
-            <Item />
-            <Item />
-            <Item />
-        </div>
-    )
-}
-
-export default List
+export default List;
